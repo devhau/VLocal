9b14137207cd24a47b2dbc703408cf2ec6e64f9f
Nguyen Van Hau <nguyenvanhau.soft@gmail.com>
2022-02-13 07:33:12 +0700

    commit first

---
+++ app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <AppWindow>
+    <div>Xin chào</div>
+    <button class="btn btn-primary" @click="test">Thử nghiệm</button>
+  </AppWindow>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AppWindow from "./components/AppWindow.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    AppWindow,
+  },
+  methods: {
+    test() {
+      console.log("1234");
+    },
+  },
+});
+</script>
+
+<style></style>
---
+++ app/src/components/AppWindow.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="mainApp">
+    <div class="topBar">
+      <span class="icon"> <i class="bi bi-app" /></span>
+      <h3 class="title">Xin chào</h3>
+    </div>
+  </div>
+</template>
+<style scoped>
+.mainApp {
+  background: #ccc;
+  width: 100%;
+  height: 100%;
+}
+.topBar {
+  height: 40px;
+  width: 100%;
+  background: #111;
+  display: flex;
+  flex-direction: row;
+}
+.topBar .title {
+  -webkit-app-region: drag;
+  padding: 3px 10px;
+  color: #fff;
+  line-height: 1.4;
+  flex: auto;
+  font-weight: 600;
+}
+.topBar .icon {
+  text-align: center;
+  width: 35px;
+  padding: 5px;
+  background: #ccc;
+}
+</style>
